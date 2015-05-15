749cc6bfb2e6167eaf16ecb060f721ef37285372
Leandro Ostera <leandro@ostera.io>
2015-05-15 20:51:42 -0300

    Adds normalize function to split delay in chunks

--- src/event.erl
+++ src/event.erl
@@ -12,3 +12,16 @@ loop(S = #state{server=Server, delay=Delay}) ->
   after Delay*1000 ->
     Server ! {done, S#state.name}
   end.
+
+%% Erlang is limited to about 49 days (49*24*60*60*1000) of wait
+%% in an after statement, so we need to split any time into 49 days
+%% parts!
+normalize(N) ->
+  %% 49 days limit
+  Limit = 49*24*60*60,
+  %% lists:duplicate(times, value).
+  %% lists:duplicate(3, a) == [a,a,a].
+  %% this generates a list of as many times Limit as
+  %% N fits in Limit as a tail, and a head of the reminder
+  %% of the division between N and Limit.
+  [N rem Limit | lists:duplicate(N div Limit, Limit)].
