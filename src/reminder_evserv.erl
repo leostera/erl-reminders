eeb0eafd1dcf65f7d474520a222796bf790a61bc
Leandro Ostera <leandro@ostera.io>
2015-05-15 22:17:29 -0300

    Adds date and time validation functions

--- src/reminder_evserv.erl
+++ src/reminder_evserv.erl
@@ -9,13 +9,20 @@
                 pid,
                 timeout={{1970,1,1},{0,0,0}}}).
 
+init() ->
+  loop(#state{events=orddict:new(),
+              clients=orddict:new()}).
+
 loop(S= #state{events=Events, clients=Clients}) ->
   receive
     {Pid, MsgRef, {subscribe, Client}} ->
-      ;
+      Ref = erlang:monitor(process, Client),
+      NewClients = orddict:store(Ref, Client, S#state.clients),
+      Pid ! {MsgRef, ok},
+      loop(S#state{clients=NewClients});
     {Pid, MsgRef, {add, Name, Description, TimeOut}} ->
       ;
-    {Pid, MsgRef, {cancle, Name}} ->
+    {Pid, MsgRef, {cancel, Name}} ->
       ;
     shutdown ->
       ;
@@ -27,3 +34,21 @@ loop(S= #state{events=Events, clients=Clients}) ->
       io:format("Unknown message: ~p~n", [Unknown]),
       loop(State)
   end.
+
+
+%% private date and time validation functions
+
+valid_datetime({Date, Time}) ->
+  try
+    calendar:valid_date(Date) andalso valid_time(Time)
+  catch
+    error:function_clause ->
+      false
+  end;
+valid_datetime(_) -> false.
+
+valid_time({H,M,S}) -> valid_time(H,M,S).
+valid_time(H,M,S) when H >= 0, H < 24,
+                       M >= 0, M < 60,
+                       S >= 0, S < 60 -> true;
+valid_time(_,_,_) -> false.
