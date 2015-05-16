2673cfc2b2f5e79ae2a3073ab45191e21ef699bc
Leandro Ostera <leandro@ostera.io>
2015-05-15 21:30:56 -0300

    Tries to add time_left API

--- src/event.erl
+++ src/event.erl
@@ -15,10 +15,13 @@ start_link(EventName, Delay) ->
 init(Server, EventName, Delay) ->
   loop(#state{server=Server,
               name=EventName,
-              delay=normalize(Delay)}).
+              delay=date_to_secs(Delay)}).
 
 loop(S = #state{server=Server, delay=[T|Next]}) ->
   receive
+    {Server, Ref, time_left} ->
+      TimeLeft = lists:foldl( fun(X,Sum) -> X+Sum end, 0, Next),
+      Server ! {TimeLeft, Ref, ok};
     {Server, Ref, cancel} ->
       Server ! {Ref, ok}
   after T*1000 ->
@@ -42,6 +45,17 @@ cancel(Pid) ->
       ok
   end.
 
+time_left(Pid) ->
+  Ref = erlang:monitor(process, Pid),
+  Pid ! {self(), Ref, time_left},
+  receive
+    {Time, Ref, ok} -> 
+      erlang:demonitor(Ref, [flush]),
+      {ok, Time};
+    {'DOWN', Ref, process, Pid, _Reason} -> 
+      ok
+  end.
+
 %% Erlang is limited to about 49 days (49*24*60*60*1000) of wait
 %% in an after statement, so we need to split any time into 49 days
 %% parts!
@@ -54,3 +68,12 @@ normalize(N) ->
   %% N fits in Limit as a tail, and a head of the reminder
   %% of the division between N and Limit.
   [N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+date_to_secs(TimeOut={{_,_,_},{_,_,_}}) ->
+  Now = calendar:local_time(),
+  ToGo = calendar:datetime_to_gregorian_seconds(TimeOut) -
+         calendar:datetime_to_gregorian_seconds(Now),
+  Secs = if ToGo > 0 -> ToGo;
+            ToGo =<0 -> 0
+         end,
+  normalize(Secs).
